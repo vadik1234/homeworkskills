226ae03c5d51f962a41dae20424e7fd6ef70507c
vadik1234 <zakharkevich1994@gmail.com>
2021-03-02 17:13:11 +0300

    fix

--- home_work11/task11_1.py
+++ home_work11/task11_1.py
@@ -7,42 +7,34 @@ class Synthesizer:
         self.__synthesis_method = synthesis_method
         self.__manufacturer = manufacturer
 
-    def play_c(self):
-        """Play c - note 3rd octave"""
-        print('Ceeeeeeeeeeee ')
+    def play_c(self):        
+        print('Ceeee')
 
-    def puls_width_modulation(self):
-        """Run PWD"""
-        print('Shhh-Bzzz  krrrrr')
+    def puls_width_modulation(self):        
+        print('Shr')
 
     @property
-    def amount_osc(self):
-        """Returns oscillators amount"""
+    def amount_osc(self):        
         return self.__amount_osc
 
     @amount_osc.setter
-    def amount_osc(self, value):
-        """Sets oscillators amount"""
+    def amount_osc(self, value):        
         self.__amount_osc = value
 
     @property
-    def synthesis_method(self):
-        """Returns type of synthesis method machine use"""
+    def synthesis_method(self):        
         return self.__synthesis_method
 
     @synthesis_method.setter
-    def synthesis_method(self, method):
-        """Sets type of synthesis method machine use"""
+    def synthesis_method(self, method):        
         self.__synthesis_method = method
 
     @property
-    def manufacturer(self):
-        """Returns machine manufacturer"""
+    def manufacturer(self):        
         return self.__manufacturer
 
     @manufacturer.setter
-    def manufacturer(self, company):
-        """Sets machine manufacturer"""
+    def manufacturer(self, company):        
         self.__manufacturer = company
 
 
@@ -60,44 +52,36 @@ class MyCat:
         self.__fav_food = fav_food
         self.__fav_sleep_place = fav_sleep_place
 
-    def want_eat_at_5am(self):
-        """The cat wants to eat in the early morning"""
+    def want_eat_at_5am(self):        
         if self.__hungry:
-            print('Meeeeeooowowww  aaaoow')
+            print('Meow ')
 
     def want_sleep(self):
-        """The cat wants to sleep"""
         if not self.__hungry:
-            print('Zzz Zzz Zzz')
+            print('Zzz')
 
     @property
-    def hungry(self):
-        """Returns how hungry cat"""
+    def hungry(self):       
         return self.__hungry
 
     @hungry.setter
-    def hungry(self, value):
-        """Sets hungry value"""
+    def hungry(self, value):        
         self.__hungry = value
 
     @property
-    def fav_food(self):
-        """Returns cats favourite food"""
+    def fav_food(self):        
         return self.__fav_food
 
     @fav_food.setter
-    def fav_food(self, food):
-        """Sets cats fvourite food"""
+    def fav_food(self, food):       
         self.__fav_food = food
 
     @property
     def fav_sleep_place(self):
-        """Returns cats favourite sleep place"""
         return self.__fav_sleep_place
 
     @fav_sleep_place.setter
     def fav_sleep_place(self, place):
-        """Sets cats favourite sleep place"""
         self.__fav_sleep_place = place
 
 
@@ -113,43 +97,35 @@ class MyBook:
         self.pages = pages
         self.knowledge = knowledge
 
-    def lie_on_the_shelf(self):
-        """Retuns boolean value in dependeing by subject"""
+    def lie_on_the_shelf(self):        
         return True if self.__subject == 'belles-lettres' else False
 
     def give_knowledge(self):
-        """Adds value to knowledge attribute"""
         if self.__subject == 'philosophy':
             self.knowledge += 20
 
     @property
     def subject(self):
-        """Returns what subject is the book"""
         return self.__subject
 
     @subject.setter
     def subject(self, value):
-        """Sets subjects book"""
         self.__subject = value
 
     @property
     def name(self):
-        """Returns books title"""
         return self.__name
 
     @name.setter
     def name(self, value):
-        """Sets books title"""
         self.__name = value
 
     @property
     def author(self):
-        """Returns books author"""
         return self.__author
 
     @author.setter
     def author(self, value):
-        """Sets books author"""
         self.__author = value
 
 
@@ -169,44 +145,36 @@ class MyCity:
         self.__underground = underground
 
     def enlargement(self):
-        """Building up a new district"""
         self.__square += 10
         self.__population += 10000
 
     def emigration(self):
-        """Outflow of population in the absence of metro"""
         if not self.__underground:
             self.__population -= 10000
             self.__square -= 10
 
     @property
     def population(self):
-        """Returns cities population"""
         return self.__population
 
     @population.setter
     def population(self, value):
-        """Sets cities population"""
         self.__population = value
 
     @property
     def square(self):
-        """Returns cities square"""
         return self.__square
 
     @square.setter
     def square(self, value):
-        """Sets cities square"""
         self.__square = value
 
     @property
     def underground(self):
-        """Returns is there a metro in the city"""
         return self.__underground
 
     @underground.setter
     def underground(self, value):
-        """Sets whether the metro will be in the city"""
         self.__underground = value
 
 
@@ -241,43 +209,35 @@ class Oscillator:
 
     @property
     def sub_osc(self):
-        """Returns True if sub oscillator on, False otherwise"""
         return self.__sub_osc
 
     @sub_osc.setter
     def sub_osc(self, value):
-        """Sets sub oscillator on/off"""
         self.__sub_osc = value
 
     @property
     def sub_wave(self):
-        """Returns sub wave form if sub oscillator work"""
         return self.__sub_wave
 
     @sub_wave.setter
     def sub_wave(self, value):
-        """Sets sub oscillators wave form"""
         if self.__sub_osc:
             self.__sub_wave = value
 
     @property
     def wave(self):
-        """Returns oscillators wave form"""
         return self.__wave
 
     @wave.setter
     def wave(self, form):
-        """Sets oscillators wave form"""
         self.__wave = form
 
     @property
     def vibrato(self):
-        """Returns True if vibrato on, False otherwise"""
         return self.__vibrato
 
     @vibrato.setter
     def vibrato(self, value):
-        """Sets vibratos on/off"""
         self.__vibrato = value
 
 
